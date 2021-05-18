99676f490e663687dfa470623d75ce64aabddd1b
Reimund <rbayerlein@ucdavis.edu>
2021-05-18 11:48:10 -0700

    Minor changes and last commit before making the transition to multi-bed imaging.

--- show_crys_eff.m
+++ show_crys_eff.m
@@ -1,4 +1,4 @@
-fid = fopen('crys_eff_readIn_fromServer2', 'rb');
+fid = fopen('crys_eff_679x840_orig', 'rb');
 %fseek(fid, 3095517*4, 'bof'); 
 crys_eff = fread(fid, 672*840, 'float');
 crys_eff = reshape(crys_eff, 672, 840);
